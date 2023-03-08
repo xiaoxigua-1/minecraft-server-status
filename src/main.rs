0244bf6dd7f688000e0c01acb22f6a2b12711c90
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2023-03-08 12:58:49 +0000

    fix code

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,17 @@ async fn main() -> std::io::Result<()> {
     let mut buf = [0u8; 1024];
     
     loop {
-        let (len, addr) = socket.recv_from(&mut buf).expect("");
+        let (len, _) = socket.recv_from(&mut buf).expect("");
         let content = str::from_utf8(&buf[..len - 1]).unwrap();
         let content = String::from(content);
         let udp_socket = socket.clone();
         tokio::spawn(async move {
-            println!("{}", content);
             let send_buf = if let Ok(_) = TcpStream::connect(content) {
                 "Up"
             } else {
                 "Down"
             }.as_bytes();
-            
-            udp_socket.send_to(send_buf, addr).expect("");
+            udp_socket.send(send_buf).expect("");
         });
     }
 }
